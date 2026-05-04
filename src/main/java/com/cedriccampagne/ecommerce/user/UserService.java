5d548091e4abb4eb13f37c6d4b2a80679b18a165
CedricCampagne <ced.campagne@gmail.com>
2026-05-04 13:45:41 +0200

    chore(security): retirer temporairement Spring Security pour permettre les tests du module User

--- src/main/java/com/cedriccampagne/ecommerce/user/UserService.java
+++ src/main/java/com/cedriccampagne/ecommerce/user/UserService.java
@@ -2,7 +2,9 @@ package com.cedriccampagne.ecommerce.user;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.cedriccampagne.ecommerce.user.dto.UserCreateDto;
 import com.cedriccampagne.ecommerce.user.dto.UserDto;
@@ -25,7 +27,10 @@ public class UserService {
 
     public UserDto getUserById(Long id) {
         User user = userRepository.findById(id)
-        .orElseThrow(() -> new RuntimeException("Utilisateur introuvable"));
+        .orElseThrow(() -> new ResponseStatusException(
+            HttpStatus.NOT_FOUND,
+            "Utilisateur introuvable"
+        ));
 
         return UserMapper.toUserDto(user);
     }
